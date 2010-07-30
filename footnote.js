d8aca9faa375cb9b9041300229e6996f26af2679
Carl Fürstenberg <carl@excito.com>
2010-07-30 23:14:02 +0200

    Initial add

---
+++ footnote.js
@@ -0,0 +1,22 @@
+$(function(){
+		var box = $('<div/>', { 'style': 'border: 1px solid gray; position: absolute;'}).appendTo('body');
+		$('.reference').hover(
+			function(){
+				$(body).bind('mousemove.footnotehover',function(e){
+						box.position({
+								'my': 'top center',
+								'at': 'center',
+								'of': e
+							}
+						);
+					}
+				);
+				box.html($($(this).attr('href')).clone()).show();
+			},
+			function(){
+				$(body).unbind('mousemove.footnotehover');
+				box.hide().empty();
+			}
+		);
+	}
+);
